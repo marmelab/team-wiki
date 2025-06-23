8c7a83814b63bc9e1a021ff72dcbb6578387173c
Matthieu Hochlander <matthieu@marmelab.com>
2025-06-23 16:07:15 +0200

    Use b&w theme.

--- src/theme.ts
+++ src/theme.ts
@@ -1,8 +1,8 @@
-import { defaultLightTheme } from "react-admin";
+import { bwLightTheme } from "react-admin";
 import { ThemeOptions } from "@mui/material";
 import deepmerge from "deepmerge";
 
-export const appTheme: ThemeOptions = deepmerge(defaultLightTheme, {
+export const appTheme: ThemeOptions = deepmerge(bwLightTheme, {
   components: {
     RaMarkdownField: {
       styleOverrides: {
