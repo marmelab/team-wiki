e4f396562756167f5f09da2c1403673f71912c28
Matthieu Hochlander <matthieu@marmelab.com>
2025-06-23 16:40:13 +0200

    Add the list of recent revisions on homepage.

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,10 +1,44 @@
-import { CardContent, CardHeader } from "@mui/material";
+import { CardContent, CardHeader, Stack, Typography } from "@mui/material";
 import { MarkdownField } from "@react-admin/ra-markdown";
-import { FunctionField, Show } from "react-admin";
+import {
+  DateField,
+  FunctionField,
+  Show,
+  SimpleList,
+  useGetList,
+} from "react-admin";
+
+export function HomepageSidebar() {
+  const recentRevisions = useGetList("pages_history", {
+    sort: {
+      field: "date",
+      order: "DESC",
+    },
+    pagination: {
+      page: 1,
+      perPage: 10,
+    },
+  });
+
+  return (
+    <Stack sx={{ margin: "0 1em", maxWidth: "40%" }}>
+      <Typography variant="h4">Recent revisions</Typography>
+      <SimpleList
+        data={recentRevisions.data}
+        linkType={(record) => `/pages/${record.data.id}/show`}
+        primaryText={(record) => record.data.title}
+        secondaryText={(record) => record.message}
+        tertiaryText={(record) => (
+          <DateField record={record} source="date" showTime />
+        )}
+      />
+    </Stack>
+  );
+}
 
 export function Homepage() {
   return (
-    <Show title="Homepage" resource="pages" id="0">
+    <Show title="Homepage" resource="pages" id="0" aside={<HomepageSidebar />}>
       <FunctionField
         source="title"
         render={({ title }) => <CardHeader title={title} />}
