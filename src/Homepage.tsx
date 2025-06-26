895257e3a53b0d5f49982048600a9c878255aff8
François Zaninotto <fzaninotto@gmail.com>
2025-06-26 22:30:54 +0200

    Update latest changes in homepage

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,34 +1,25 @@
-import { CardContent, CardHeader, Stack, Typography } from "@mui/material";
+import {
+  Avatar,
+  CardContent,
+  CardHeader,
+  Stack,
+  Typography,
+} from "@mui/material";
 import { MarkdownField } from "@react-admin/ra-markdown";
 import {
   DateField,
-  FunctionField,
+  TextField,
   Link,
   ListBase,
   Show,
-  SimpleList,
+  WithRecord,
+  SingleFieldList,
+  WithListContext,
+  RecordContextProvider,
+  ReferenceField,
+  RecordRepresentation,
+  useRecordContext,
 } from "react-admin";
-
-export const HomepageSidebar = () => (
-  <Stack sx={{ margin: "0 1em", maxWidth: "30%" }}>
-    <Typography variant="h5">Recent revisions</Typography>
-    <ListBase
-      resource="pages_history"
-      sort={{ field: "date", order: "DESC" }}
-      disableSyncWithLocation
-    >
-      <SimpleList
-        rowClick={(_id, _resource, record) => `/pages/${record.data.id}/show`}
-        primaryText={(record) => record.data.title}
-        secondaryText={(record) => record.message}
-        tertiaryText={(record) => (
-          <DateField record={record} source="date" showTime />
-        )}
-        disablePadding
-      />
-    </ListBase>
-  </Stack>
-);
 
 export const Homepage = () => (
   <Show
@@ -43,12 +34,56 @@ export const Homepage = () => (
       </Stack>
     }
   >
-    <FunctionField
-      source="title"
-      render={({ title }) => <CardHeader title={title} />}
-    />
+    <WithRecord render={({ title }) => <CardHeader title={title} />} />
     <CardContent>
       <MarkdownField source="content" />
     </CardContent>
   </Show>
 );
+
+const HomepageSidebar = () => (
+  <Stack sx={{ margin: "0 1em", maxWidth: "30%" }}>
+    <Typography variant="h5" gutterBottom>
+      Recent revisions
+    </Typography>
+    <ListBase
+      resource="pages_history"
+      sort={{ field: "date", order: "DESC" }}
+      disableSyncWithLocation
+    >
+      <SingleFieldList gap={2} linkType={false}>
+        <Change />
+      </SingleFieldList>
+    </ListBase>
+  </Stack>
+);
+
+const Change = () => {
+  const record = useRecordContext();
+  return (
+    <Link to={`/pages/${record?.data.id}/show`} sx={{ textDecoration: "none" }}>
+      <TextField
+        source="data.title"
+        variant="body1"
+        component="div"
+        sx={{ fontWeight: "600" }}
+      />
+      <TextField source="message" variant="body1" color="textDisabled" />
+      <Stack direction="row" gap={1} sx={{ alignItems: "center" }}>
+        <ReferenceField source="authorId" reference="users" link={false}>
+          <WithRecord
+            render={(record: any) => (
+              <Avatar src={record.avatar} sx={{ width: 24, height: 24 }} />
+            )}
+          />
+        </ReferenceField>
+        <ReferenceField source="authorId" reference="users" link={false}>
+          <Typography color="textDisabled">
+            <RecordRepresentation /> on
+          </Typography>
+        </ReferenceField>
+        <DateField source="date" color="textDisabled" variant="body1" />
+      </Stack>
+    </Link>
+  );
+};
