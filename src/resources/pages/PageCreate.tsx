cf93617802b32bed706fd354c729008142a7878b
fzaninotto <fzaninotto@gmail.com>
2025-06-27 18:22:26 +0200

    improve PageCreate form

--- src/resources/pages/PageCreate.tsx
+++ src/resources/pages/PageCreate.tsx
@@ -1,14 +1,20 @@
 import { Create, required, TextInput } from "react-admin";
 import { SimpleFormWithRevision } from "@react-admin/ra-history";
-import { PageMarkdownInput } from "../../inputs/PageMarkdownInput.tsx";
 import { ReferenceNodeInput } from "@react-admin/ra-tree";
+import { Stack } from "@mui/material";
+
+import { PageMarkdownInput } from "../../inputs/PageMarkdownInput.tsx";
 
 export const PageCreate = () => (
-  <Create>
+  <Create redirect="show">
     <SimpleFormWithRevision>
-      <TextInput source="title" validate={required()} />
-      <PageMarkdownInput source="content" />
-      <ReferenceNodeInput source="category_id" reference="categories" />
+      <Stack direction="row" spacing={2} alignItems="top">
+        <div>
+          <TextInput source="title" validate={required()} helperText={false} />
+          <PageMarkdownInput source="content" helperText={false} />
+        </div>
+        <ReferenceNodeInput source="category_id" reference="categories" />
+      </Stack>
     </SimpleFormWithRevision>
   </Create>
 );
